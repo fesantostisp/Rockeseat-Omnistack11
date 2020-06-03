0d6e9e1703fa9134275b6de087fde1ab58422b7f
fmsantosti21 <felipe_matheus.20@hotmail.com>
2020-06-03 19:45:20 -0300

    Validação Celebrate

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express'); 
 const cors = require('cors');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());  //modulo http e Web Service
 app.use(routes);
+app.use(errors());
+
 
 app.listen(3333);
